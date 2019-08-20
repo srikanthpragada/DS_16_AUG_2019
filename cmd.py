865e0eeb36029c344b07ee35e3a76c220d7c9e36
srikanthpragada <srikanthpragada@gmail.com>
2019-08-20 17:58:17 +0530

    20-AUG

---
+++ cmd.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+b = 10 
+print(a)
+a = 10
+b = 10 
+print(a)
+for i in range(1,10):
+   print(i * i)               
+import datetime 
+print(datetime.datetime.now())
+v = in
+print(datetime.datetime.now())
+print(datetime.datetime.now())
+print(datetime.datetime.now())
